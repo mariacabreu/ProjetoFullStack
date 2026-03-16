ec35feab83fbbefe2310148d2d3f06132d7b0b0e
Seu Nome <seuemail@exemplo.com>
2026-03-16 20:48:31 -0300

    update settings client model

--- index.js
+++ index.js
@@ -3,7 +3,23 @@ const cors = require ('cors')
 const { Sequelize, DataTypes } = require ('sequelize')
 
 //Confriguração do banco de dados
-const sequelize = new Sequelize ('db_projeto', 'root', '', {
+const sequelize = new Sequelize ('project', 'root', '', {
     host: 'localhost',
     dialect: 'mysql'
+})
+
+//Definição do modelo de cliente
+const Cliente = sequelize.define('Cliente', {
+    nome: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    telefone: {
+        type: DataTypes.STRING,
+    }
 })
